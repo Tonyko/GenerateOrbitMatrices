b2de666271b310fa5fc2b757cc8094a0f0cb3385
Tony <aktealc@gmail.com>
2015-10-07 21:58:15 +0200

    finished generating rows

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 #include "libraryZ3.hpp"
 #include "math.hpp"
@@ -7,12 +8,12 @@
 using namespace std;
 
 // strongly regular graph parameters
-int n = 15;
-int k = 6;
-int l = 1;
-int m = 3;
+int n = 25;
+int k = 12;
+int l = 5;
+int m = 6;
 int p = 3;
-int f = 3;
+int f = 1;
 int o = (n - f) / p;
 
 int isoConst = 5;
@@ -21,15 +22,15 @@ vector<vector<int>> linEqFixSolution;
 vector<vector<int>> linEqOrbitSolution0;
 vector<vector<int>> linEqOrbitSolution2;
 
-vector<vector<int>> solutions;
+vector<vector<vector<int>>> solutions;
 
 // const
 vector<vector<int>> iMatrix;
 vector<vector<int>> jMatrix;
 vector<vector<int>> constMatrix;
 vector<int> nMatrix;
-vector<int> lambdaSets;
-vector<int> muSets;
+vector<int> lambdaSet;
+vector<int> muSet;
 
 // from Behbahani thesis
 void solve_linear_equations() {
@@ -101,13 +102,13 @@ void const_matrices() {
 
     int i = l % p;
     while (i <= n) {
-      lambdaSets.push_back(i);
+      lambdaSet.push_back(i);
       i += p;
     }
 
     int ii = m % p;
     while (ii <= n) {
-      muSets.push_back(ii);
+      muSet.push_back(ii);
       ii += p;
     }
 }
@@ -150,9 +151,212 @@ vector<vector<int>> orbit_col_permutation(vector<int> row, vector<vector<int>> i
     return tmpMatrix;
 }
 
+vector<int> print_cert_matrix(vector<vector<int>> matrix) {
+    vector<int> tmpVector;
+    for (int i=0; i<matrix.size(); i++) {
+        for(int j=i; j<matrix[i].size(); j++) {
+            tmpVector.push_back(matrix[i][j]);
+        }
+    }
+
+    return tmpVector;
+}
 
 void generate_orbit_matrix(vector<vector<int>> matrix, int row, vector<vector<int>> indexes) {
 
+    int numberFix0 = 0;
+    int number0 = 0;
+    int number1 = 0;
+    int number2 = 0;
+    int number3 = 0;
+
+    vector<int> tmpSol;
+    for (int i=0; i<f; i++) {
+        if (matrix[row][i] == 0) numberFix0++;
+        tmpSol.push_back(matrix[row][i]);
+    }
+    for (int i=f; i<row; i++) {
+        if (matrix[row][i] == 0) number0++;
+        if (matrix[row][i] == 1) number1++;
+        if (matrix[row][i] == 2) number2++;
+        if (matrix[row][i] == 3) number3++;
+        tmpSol.push_back(matrix[row][i]);
+    }
+    // create lin. equations from matrix equation -> Behbahani
+    // s_ir = mi n_i n_r + (l - m) c_ir n_r
+    // s_ir = sum_k 1 to o {c_ik c_rk n_k}
+    // number of lin. equations = row - 1 (s_12, s13)
+
+    vector<vector<int>> linEquations;
+    vector<int> vect;
+
+    for (int i=0; i<row; i++) {
+        int value = (m * nMatrix[i] * nMatrix[row]) + ((l -m) * nMatrix[row] * matrix[i][row]);
+
+        vector<int> equation;
+        for (int j=0; j<(f+o)-row; j++) equation.push_back(0);
+
+        for (int j=0; j<row; j++) {
+            if (tmpSol[j] != 0) value -= tmpSol[j] * matrix[i][j] * nMatrix[j];
+        }
+
+        for (int j=row; j<f+o; j++) {
+            equation[j - row] = matrix[i][j] * nMatrix[j];
+        }
+
+        vect.push_back(value);
+        linEquations.push_back(equation);
+    }
+
+    vector<vector<int>> sol0 = solve_linear_equations_with_constraints_orbit(linEquations, vect, 0, 3, 0);
+    for (int i=0; i<sol0.size(); i++) {
+        vector<int> possibleRow;
+        for (int j=0; j<tmpSol.size(); j++) possibleRow.push_back(tmpSol[j]);
+        for (int j=0; j<sol0[i].size(); j++) possibleRow.push_back(sol0[i][j]);
+
+        int number00 = 0;
+        int number11 = 0;
+        int number22 = 0;
+        int number33 = 0;
+
+        bool testSeq = false;
+        for (int j=0; j<indexes.size(); j++) {
+            int num = -1;
+            if (testSeq == true) continue;
+            for (int k=0; k<indexes[j].size(); k++) {
+                if (indexes[j][k] + f > row) {
+                    if (possibleRow[f + indexes[j][k]] >= num) {
+                        num = possibleRow[f + indexes[j][k]];
+                        if (possibleRow[f + indexes[j][k]] == 0) number00++;
+                        if (possibleRow[f + indexes[j][k]] == 1) number11++;
+                        if (possibleRow[f + indexes[j][k]] == 2) number22++;
+                        if (possibleRow[f + indexes[j][k]] == 3) number33++;
+                    }
+                    else testSeq = true;
+                }
+            }
+        }
+        if (testSeq == true) continue;
+
+        for (int jj=0; jj<linEqOrbitSolution0.size(); jj++) {
+            if (linEqOrbitSolution0[jj][0] == numberFix0 &&
+                linEqOrbitSolution0[jj][1] == f-numberFix0 &&
+                linEqOrbitSolution0[jj][2] == number0 + number00 + 1 &&
+                linEqOrbitSolution0[jj][3] == number1 + number11 &&
+                linEqOrbitSolution0[jj][4] == number2 + number22 &&
+                linEqOrbitSolution0[jj][5] == number3 + number33) {
+                
+                matrix[row] = possibleRow;
+                for (int j=row+1; j<f+o; j++) matrix[j][row] = matrix[row][j];
+                
+                // test on isomorphism
+                bool testIso = false;
+                vector<int> cert = print_cert_matrix(matrix);
+                for (int j=0; j<indexes.size(); j++) {
+                    if (testIso == true) break;
+                    if (indexes[j].size() != 1) {
+                        vector<int> tmp = indexes[j];
+                        do {
+                            vector<int> listPermutation = create_vector(f + o);
+                            for (int k=0; k<indexes[j].size(); k++) listPermutation[f + indexes[j][k]] = f + tmp[k];
+                            vector<int> cert2;
+                            for (int k=0; k<matrix.size(); k++) {
+                                for (int l=k; l<matrix.size(); l++) cert2.push_back(matrix[listPermutation[k]][listPermutation[l]]);
+                            }
+                            if (operator<(cert2, cert)) {
+                                testIso = true;
+                                break;
+                            }
+                        } while(next_permutation(tmp.begin(),tmp.end()));
+                    }
+                }
+                if (testIso == true) continue;
+
+                if (row + 1 == f + o) {
+                    solutions.push_back(matrix);
+                }
+                else {
+                    vector<int> orbitRow(possibleRow.begin() + f, possibleRow.end());
+                    generate_orbit_matrix(matrix, row + 1, orbit_col_permutation(orbitRow, indexes));
+                }
+            }
+        }
+    }
+
+    vector<vector<int>> sol2 = solve_linear_equations_with_constraints_orbit(linEquations, vect, 0, 3, 2);
+    for (int i=0; i<sol2.size(); i++) {
+        vector<int> possibleRow;
+        for (int j=0; j<tmpSol.size(); j++) possibleRow.push_back(tmpSol[j]);
+        for (int j=0; j<sol2[i].size(); j++) possibleRow.push_back(sol2[i][j]);
+
+        int number00 = 0;
+        int number11 = 0;
+        int number22 = 0;
+        int number33 = 0;
+
+        bool testSeq = false;
+        for (int j=0; j<indexes.size(); j++) {
+            int num = -1;
+            if (testSeq == true) continue;
+            for (int k=0; k<indexes[j].size(); k++) {
+                if (indexes[j][k] + f > row) {
+                    if (possibleRow[f + indexes[j][k]] >= num) {
+                        num = possibleRow[f + indexes[j][k]];
+                        if (possibleRow[f + indexes[j][k]] == 0) number00++;
+                        if (possibleRow[f + indexes[j][k]] == 1) number11++;
+                        if (possibleRow[f + indexes[j][k]] == 2) number22++;
+                        if (possibleRow[f + indexes[j][k]] == 3) number33++;
+                    }
+                    else testSeq = true;
+                }
+            }
+        }
+        if (testSeq == true) continue;
+
+        for (int jj=0; jj<linEqOrbitSolution2.size(); jj++) {
+            if (linEqOrbitSolution2[jj][0] == numberFix0 &&
+                linEqOrbitSolution2[jj][1] == f-numberFix0 &&
+                linEqOrbitSolution2[jj][2] == number0 + number00 &&
+                linEqOrbitSolution2[jj][3] == number1 + number11 &&
+                linEqOrbitSolution2[jj][4] == number2 + number22 + 1 &&
+                linEqOrbitSolution2[jj][5] == number3 + number33) {
+                
+                matrix[row] = possibleRow;
+                for (int j=row+1; j<f+o; j++) matrix[j][row] = matrix[row][j];
+                
+                // test on isomorphism
+                bool testIso = false;
+                vector<int> cert = print_cert_matrix(matrix);
+                for (int j=0; j<indexes.size(); j++) {
+                    if (testIso == true) break;
+                    if (indexes[j].size() != 1) {
+                        vector<int> tmp = indexes[j];
+                        do {
+                            vector<int> listPermutation = create_vector(f + o);
+                            for (int k=0; k<indexes[j].size(); k++) listPermutation[f + indexes[j][k]] = f + tmp[k];
+                            vector<int> cert2;
+                            for (int k=0; k<matrix.size(); k++) {
+                                for (int l=k; l<matrix.size(); l++) cert2.push_back(matrix[listPermutation[k]][listPermutation[l]]);
+                            }
+                            if (operator<(cert2, cert)) {
+                                testIso = true;
+                                break;
+                            }
+                        } while(next_permutation(tmp.begin(),tmp.end()));
+                    }
+                }
+                if (testIso == true) continue;
+
+                if (row + 1 == f + o) {
+                    solutions.push_back(matrix);
+                }
+                else {
+                    vector<int> orbitRow(possibleRow.begin() + f, possibleRow.end());
+                    generate_orbit_matrix(matrix, row + 1, orbit_col_permutation(orbitRow, indexes));
+                }
+            }
+        }
+    }
 }
 
 void generate_fix_matrices(vector<vector<int>> matrix, int row, int fixDegree, int orbitDegree, vector<vector<int>> fixIndexes, vector<vector<int>> orbitIndexes) {
@@ -253,7 +457,7 @@ void generate_fix_matrices(vector<vector<int>> matrix, int row, int fixDegree, i
                 int num = -1;
                 if (testSeq == true) continue;
                 for (int k=0; k<orbitIndexes[j].size(); k++) {
-                    if (orbitIndexes[j][k] > row) {
+                    if (f + orbitIndexes[j][k] > row) {
                         if (possibleRow[f + orbitIndexes[j][k]] >= num) {
                             num = possibleRow[f + orbitIndexes[j][k]];
                             sumFix += possibleRow[f + orbitIndexes[j][k]];
@@ -273,11 +477,87 @@ void generate_fix_matrices(vector<vector<int>> matrix, int row, int fixDegree, i
             }
 
             // lambda/ mu test
+            bool testLM = false;
             for (int j=0; j<row; j++) {
                 int count = 0;
-                
-            }
 
+                for (int k=0; k<f; k++) {
+                    if ((matrix[row][k] == 1) && (matrix[j][k] == 1)) count++;
+                }
+
+                if (matrix[j][row] == 1) {
+                    // lambdaSet
+                    if(std::find(lambdaSet.begin(), lambdaSet.end(), count) == lambdaSet.end()) {
+                        testLM = true;
+                    }
+                }
+                else {
+                    // muSet
+                    if(std::find(muSet.begin(), muSet.end(), count) == muSet.end()) {
+                        testLM = true;
+                    }
+                }
+            }
+            if (testLM == true) continue;
+
+            // test on isomorphism
+            bool testIso = false;
+            if (row < isoConst) {
+                vector<int> cert = print_cert_matrix(matrix);
+                
+                for (int j=0; j<fixIndexes.size(); j++) {
+                    if (testIso == true) break;
+                    if (fixIndexes[j].size() != 1) {
+                        vector<int> tmp = fixIndexes[j];
+                        do {
+                            vector<int> listPermutation = create_vector(f + o);
+                            for (int k=0; k<fixIndexes[j].size(); k++) listPermutation[fixIndexes[j][k]] = tmp[k];
+                            vector<int> cert2;
+                            for (int k=0; k<matrix.size(); k++) {
+                                for (int l=k; l<matrix.size(); l++) cert2.push_back(matrix[listPermutation[k]][listPermutation[l]]);
+                            }
+                            if (operator<(cert2, cert)) {
+                                testIso = true;
+                                break;
+                            }
+                        } while(next_permutation(tmp.begin(),tmp.end()));
+                    }
+                }
+                if (testIso == true) continue;
+
+                for (int j=0; j<orbitIndexes.size(); j++) {
+                    if (testIso == true) break;
+                    if (orbitIndexes[j].size() != 1) {
+                        vector<int> tmp = orbitIndexes[j];
+                        do {
+                            vector<int> listPermutation = create_vector(f + o);
+                            for (int k=0; k<orbitIndexes[j].size(); k++) listPermutation[f + orbitIndexes[j][k]] = f + tmp[k];
+                            vector<int> cert2;
+                            for (int k=0; k<matrix.size(); k++) {
+                                for (int l=k; l<matrix.size(); l++) cert2.push_back(matrix[listPermutation[k]][listPermutation[l]]);
+                            }
+                            if (operator<(cert2, cert)) {
+                                testIso = true;
+                                break;
+                            }
+                        } while(next_permutation(tmp.begin(),tmp.end()));
+                    }
+                }
+                if (testIso == true) continue;
+
+                if (row + 1 == f) {
+                    vector<int> orbitRow(possibleRow.begin() + f, possibleRow.end());
+                    generate_orbit_matrix(matrix, f, fix_col_permutation(orbitRow, orbitIndexes));
+                }
+                else {
+                    vector<int> fixRow(possibleRow.begin(), possibleRow.begin() + f);
+                    vector<int> orbitRow(possibleRow.begin() + f, possibleRow.end());
+                    for (int i=0; i<linEqFixSolution.size(); i++) {
+                        generate_fix_matrices(matrix, row + 1, linEqFixSolution[i][1], linEqFixSolution[i][3], fix_col_permutation(fixRow, fixIndexes),
+                         fix_col_permutation(orbitRow, orbitIndexes));
+                    }            
+                }           
+            }
         }
     }
 }
@@ -297,6 +577,8 @@ int main() {
     for (int i=0; i<linEqFixSolution.size(); i++) {
         generate_fix_matrices(create_matrix(f + o, 5), 0, linEqFixSolution[i][1], linEqFixSolution[i][3], create_indexes(f), create_indexes(o));
     }
+
+    cout << "Number of solutions: " << solutions.size() << endl;
 
     return 0;
 }
